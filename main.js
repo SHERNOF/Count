0833d13cda226a8d11701f6a244ffbb56b13e1cd
Shernof <sherwin.nofuente@yahoo.com>
2019-10-07 15:34:18 +0800

    4

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
+
+var counter = document.getElementById('counter')
 var countM = document.getElementsByClassName('col1')[0], a = 0;
 countM.onclick = function() {
     a += 1;
     document.getElementById('counter').innerHTML = "Attendees: " + a;
 }
 
-// var pauseMe = document.getElementsByClassName('pauseMe')[0];
-// pauseMe.onclick = function(e) {
-//     e.preventDefault()
-//     document.getElementById('test1').disabled = true;
-// }
+var pauseMe = document.getElementById('pauseMe');
+pauseMe.onclick = function () {
+    counter.disabled = true;
+}
 
-// var nodes = document.getElementById("test2").getElementsByTagName('*');
-// for(var i = 0; i < nodes.length; i++){
-//      nodes[i].disabled = true;
-//      console.log('testing')
-// }
+var resumeMe = document.getElementById('resumeMe');
+resumeMe.onclick = function () {
+    counter.disabled = false;
+}
 
-// $('#DisableDiv').fadeTo('slow',.6);
-// $('#DisableDiv').append('<div style="position: absolute;top:0;left:0;width: 100%;height:100%;z-index:2;opacity:0.4;filter: alpha(opacity = 50)"></div>');
+var resetMe = document.getElementById('resetMe');
+resetMe.onclick = function () {
+    x = 0
+    counter.disabled = false;
+    document.getElementById('counter').innerHTML = 'Click on Surface to Count';
+    
+}
 
-
+var saveMe = document.getElementById('saveMe').disabled = true;
